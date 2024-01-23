b83d3cd209d730c20af0da136324d14c62fc822e
Constantin <62252282+cern7@users.noreply.github.com>
2024-01-23 07:30:40 +0200

    refactor, downgrade the correctness to 70%

--- exercises/contest2015/codility/src/LongestPassword.java
+++ exercises/contest2015/codility/src/LongestPassword.java
@@ -12,39 +12,28 @@ public class LongestPassword {
         return !res.isEmpty() ? res.length() : -1;
     }
 
+    private static boolean isValidPassword(String word) {
+        int numberOfLetters = 0;
+        int numberOfDigits = 0;
+        for (char c : word.toCharArray()) {
+            if (!Character.isLetterOrDigit(c)) {
+                return false;
+            }
+            if (Character.isAlphabetic(c)) {
+                numberOfLetters++;
+            } else if (Character.isDigit(c)) {
+                numberOfDigits++;
+            }
+        }
+        return numberOfDigits % 2 != 0 && numberOfLetters % 2 == 0;
+    }
+
     private static String getPassword(String[] words) {
-        List<String> tempRes = new ArrayList<>();
-        String maxLengthStr = "";
+        List<String> validWords = Arrays.stream(words)
+                .filter(LongestPassword::isValidPassword)
+                .toList();
+        Optional<String> maxWord = validWords.stream().max(String::compareToIgnoreCase);
 
-        for (String word : words) {
-            int numberOfLetters = 0;
-            int numberOfDigits = 0;
-            boolean include = true;
-            for (char c : word.toCharArray()) {
-                if (!Character.isAlphabetic(c) && !Character.isDigit(c)) {
-                    include = false;
-                    break;
-                }
-                if (Character.isAlphabetic(c)) {
-                    numberOfLetters++;
-                } else if (Character.isDigit(c)) {
-                    numberOfDigits++;
-                }
-            }
-            if (include
-                    && numberOfDigits % 2 != 0
-                    && numberOfLetters % 2 == 0) {
-                tempRes.add(word);
-            }
-        }
-        if (tempRes.isEmpty()) {
-            return "";
-        }
-        for (String word : tempRes) {
-            maxLengthStr = maxLengthStr.length() > word.length()
-                    ? maxLengthStr : word;
-        }
-
-        return maxLengthStr;
+        return maxWord.orElse("");
     }
 }
